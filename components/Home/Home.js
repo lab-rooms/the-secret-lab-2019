d17ae179c70a219b9f77d7959a27bc1bd011616a
Joseph Lloyd <joe.lloyd.22.24@gmail.com>
2019-09-10 11:37:38 +0200

    git ignore and static

--- components/Home/Home.js
+++ components/Home/Home.js
@@ -1,19 +1,47 @@
 import React from 'react';
-import './home.scss';
-import Header from '../Header';
-import Hero from '../Hero';
-import AboutHome from '../AboutHome';
-import Footer from '../Footer';
 
 class Home extends React.Component {
   render() {
     return (
-      <>
-        <Header />
-        <Hero />
-        <AboutHome />
-        <Footer />
-      </>
+      <div className="main main--intro">
+        <div className="main__header">
+          <div className="logo">
+            <div><img src="/static/images/logo-def.png" alt="" /></div>
+          </div>
+          <div style="max-width: 620px; width: 100%; position: relative;">
+            <div className="main__intro">
+              <img src="/static/images/logo-def-black-white.png" width="300" alt="" />
+            </div>
+            <a href="#" className="button">Take me there</a>
+          </div>
+        </div>
+
+        <div className="main__content">
+          <div className="main__body">
+            <div className="main__content-mask-floating-asset" />
+            <div className="main__content-mask">
+              <p className="pre-header">small and crafty</p>
+              <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
+                placeat facilis laborum
+                ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
+                magni molestiae corporis
+                repellendus labore.</p>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
+                eveniet aspernatur
+                necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
+                ducimus nostrum quisquam ex,
+                officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
+                elit. Exercitationem odit
+                obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
+                dolorum architecto, ex
+                ut
+                molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
+              <p><label htmlFor="main-1">Toggle</label></p>
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
--- next.config.js
+++ next.config.js
@@ -1,20 +1,5 @@
 require('dotenv').config();
-const { ContentItem, DeliveryClient, TypeResolver } = require('kentico-cloud-delivery');
 const withSass = require('@zeit/next-sass');
-
-const getItems = (type, lang) => {
-  const deliveryClient = new DeliveryClient({
-    projectId: process.env.KENTICO_PROJECT_ID,
-    typeResolvers: [
-      new TypeResolver(type, () => new ContentItem()),
-    ]
-  });
-
-  return deliveryClient.items()
-  .languageParameter(lang)
-  .type(type)
-  .toPromise()
-};
 
 module.exports = withSass({
   env: {
@@ -29,36 +14,6 @@ module.exports = withSass({
       '/en': { page: '/', query: { 'lang': 'en' } },
       '/nl': { page: '/', query: { 'lang': 'nl' } },
     };
-
-    const { items: itemsEN } = await getItems('post', 'en');
-    const postsEN = itemsEN.reduce((acc, item) => {
-      return {
-        ...acc,
-        [`/en/posts/${item.slug.value}`]: {
-          page: '/[lang]/posts/[slug]',
-          query: {
-            lang: 'en',
-            slug: item.slug.value
-          }
-        }
-      }
-    }, {});
-
-    const { items: itemsNl } = await getItems('post', 'nl');
-    const postsNL = itemsNl.reduce((acc, item) => {
-      return {
-        ...acc,
-        [`/nl/posts/${item.slug.value}`]: {
-          page: '/[lang]/posts/[slug]',
-          query: {
-            lang: 'nl',
-            slug: item.slug.value
-          }
-        }
-      }
-    }, {});
-
-    routes = { ...routes, ...postsNL, ...postsEN };
 
     return routes;
   },
