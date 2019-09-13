dc238c066708900f49491b6daf60a4c70bb38d59
Lisanne Chin <lisanne.chin@gmail.com>
2019-09-13 08:57:34 +0200

    Create floating asset component

---
+++ components/FloatingAsset/FloatingAsset.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './floating-asset.scss';
+
+class FloatingAsset extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+        <div className="floating-asset"/>
+    )
+  }
+}
+
+export default FloatingAsset
