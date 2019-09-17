d207e211dd9cb698b62a64bd3cee8cd5b4edddda
Robin van Diesen <robinvandiesen@gmail.com>
2019-09-17 16:26:13 +0200

    Used components on pages

--- compositions/What/what.js
+++ compositions/What/what.js
@@ -1,27 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
-import Card from '../../components/Card';
-import FloatingAsset from '../../components/FloatingAsset';
+import CardWithImage from '../../compositions/CardWithImage';
+import Chevron from '../../components/Chevron';
 import './what.scss';
 
 class What extends React.Component {
-  constructor(props) {
-    super(props);
-    // this.state = {
-    //   animationTitle: '',
-    //   animationContent: '',
-    // };
-  }
-
-  componentDidMount() {
-    // setTimeout(() => {
-    //   this.setState({ animationTitle: 'make-small' })
-    // }, 1000);
-    // setTimeout(() => {
-    //   this.setState({ animationContent: 'reveal' })
-    // }, 2000);
-  }
-
   render() {
     return (
       <section className="what">
@@ -30,75 +12,29 @@ class What extends React.Component {
             <h1 title="What." className={'shadow shadow--float'}>
               <span>What.</span>
             </h1>
+            <Chevron />
           </div>
           <div className={'main__content'}>
             <div className="main__body">
-              <div className={'grid--max-2'}>
-                <div>
-                  <Card>
-                    <p className="pre-header">small and crafty</p>
-                    <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
-                      placeat facilis laborum
-                      ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
-                      magni molestiae corporis
+              <CardWithImage image="/static/images/contact.jpg" >
+                <p className="pre-header">small and crafty</p>
+                <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
+                  placeat facilis laborum
+                  ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
+                  magni molestiae corporis
                 repellendus labore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
-                      eveniet aspernatur
-                      necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
-                      ducimus nostrum quisquam ex,
-                      officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
-                      elit. Exercitationem odit
-                      obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
-                      dolorum architecto, ex
-                      ut
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
+                  eveniet aspernatur
+                  necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
+                  ducimus nostrum quisquam ex,
+                  officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
+                  elit. Exercitationem odit
+                  obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
+                  dolorum architecto, ex
+                  ut
                 molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
-                  </Card>
-                  <Card>
-                    <p className="pre-header">small and crafty</p>
-                    <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
-                      placeat facilis laborum
-                      ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
-                      magni molestiae corporis
-                repellendus labore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
-                      eveniet aspernatur
-                      necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
-                      ducimus nostrum quisquam ex,
-                      officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
-                      elit. Exercitationem odit
-                      obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
-                      dolorum architecto, ex
-                      ut
-                molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
-                  </Card>
-                  <Card>
-                    <p className="pre-header">small and crafty</p>
-                    <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
-                      placeat facilis laborum
-                      ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
-                      magni molestiae corporis
-                repellendus labore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
-                      eveniet aspernatur
-                      necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
-                      ducimus nostrum quisquam ex,
-                      officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
-                      elit. Exercitationem odit
-                      obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
-                      dolorum architecto, ex
-                      ut
-                molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
-                  </Card>
-                </div>
-                <div>
-                  <FloatingAsset
-                    image={'/static/images/contact.jpg'}
-                  />
-                </div>
-              </div>
+              </CardWithImage>
             </div>
           </div>
         </div>
--- compositions/Who/Who.js
+++ compositions/Who/Who.js
@@ -1,86 +1,40 @@
 import React from 'react';
-import Card from '../../components/Card';
-import FloatingAsset from '../../components/FloatingAsset';
+import CardWithImage from '../../compositions/CardWithImage';
+import Chevron from '../../components/Chevron';
 import './who.scss';
 
 class Who extends React.Component {
   render() {
     return (
       <section className="who">
-        <div className={'section__inner'}>
-          <div className={'main__header'}>
-            <h1 title="Who." className={'shadow shadow--float'}>
+        <div className={'main__header'}>
+          <img src="/static/images/who.png" />
+          {/* <h1 title="Who." className={'shadow shadow--float'}>
               <span>Who.</span>
-            </h1>
-          </div>
+            </h1> */}
+          <Chevron />
+        </div>
+        <div className={'section__inner'}>
           <div className={'main__content'}>
             <div className="main__body">
-              <div className={'grid--max-2'}>
-                <div>
-                  <Card>
-                    <p className="pre-header">small and crafty</p>
-                    <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
-                      placeat facilis laborum
-                      ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
-                      magni molestiae corporis
+              <CardWithImage image="/static/images/contact.jpg" >
+                <p className="pre-header">small and crafty</p>
+                <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
+                  placeat facilis laborum
+                  ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
+                  magni molestiae corporis
                 repellendus labore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
-                      eveniet aspernatur
-                      necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
-                      ducimus nostrum quisquam ex,
-                      officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
-                      elit. Exercitationem odit
-                      obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
-                      dolorum architecto, ex
-                      ut
-                molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
-                  </Card>
-                  <Card>
-                    <p className="pre-header">small and crafty</p>
-                    <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
-                      placeat facilis laborum
-                      ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
-                      magni molestiae corporis
+              </CardWithImage>
+              <CardWithImage image="/static/images/contact.jpg" position="card-right">
+                <p className="pre-header">small and crafty</p>
+                <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
+                  placeat facilis laborum
+                  ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
+                  magni molestiae corporis
                 repellendus labore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
-                      eveniet aspernatur
-                      necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
-                      ducimus nostrum quisquam ex,
-                      officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
-                      elit. Exercitationem odit
-                      obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
-                      dolorum architecto, ex
-                      ut
-                molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
-                  </Card>
-                  <Card>
-                    <p className="pre-header">small and crafty</p>
-                    <h2>The Secret Lab is an award winning creative agency specialising in design.</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates molestias quia
-                      placeat facilis laborum
-                      ipsum accusamus in fuga numquam, dignissimos quaerat pariatur fugiat nostrum vero
-                      magni molestiae corporis
-                repellendus labore.</p>
-                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Libero voluptatem error
-                      eveniet aspernatur
-                      necessitatibus illo tempora incidunt vitae facilis dolor? Hic natus distinctio
-                      ducimus nostrum quisquam ex,
-                      officia vitae exercitationem. Lorem ipsum dolor sit, amet consectetur adipisicing
-                      elit. Exercitationem odit
-                      obcaecati officiis temporibus itaque delectus necessitatibus consequuntur laboriosam
-                      dolorum architecto, ex
-                      ut
-                molestiae maiores, fuga libero! Dolorum repellendus in sunt?</p>
-                  </Card>
-                </div>
-                <div>
-                  <FloatingAsset
-                    image={'/static/images/contact.jpg'}
-                  />
-                </div>
-              </div>
+              </CardWithImage>
             </div>
           </div>
         </div>
