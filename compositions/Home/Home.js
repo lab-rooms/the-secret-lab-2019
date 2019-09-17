df91ddfbefef4d77154a3623a850128904bc0efb
Robin van Diesen <robinvandiesen@gmail.com>
2019-09-17 16:25:44 +0200

    Added image to home page

--- compositions/Home/Home.js
+++ compositions/Home/Home.js
@@ -1,13 +1,12 @@
 import './home.scss';
 import React from 'react';
-import Link from 'next/link';
 
 class Home extends React.Component {
   render() {
     return (
       <section className="home">
         <div className={'main__header'}>
-          <img src="/static/images/logo-def-black-white.png" width="300" alt="" />
+          <img src="/static/images/home.png" width="300" alt="" />
         </div>
       </section>
     )
