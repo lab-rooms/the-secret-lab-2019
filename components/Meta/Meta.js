a9e8470c4149324ce1e98927e26016048144a728
Robin van Diesen <robinvandiesen@gmail.com>
2019-09-20 14:53:51 +0200

    Added favicon

---
+++ components/Meta/Meta.js
@@ -0,0 +1,50 @@
+import React, { useState } from 'react';
+import Head from 'next/head';
+
+const Meta = () => {
+  return (
+    <Head>
+      <title>The secret lab</title>
+  <link rel="shortcut icon" href="/static/images/favicon/favicon.ico" type="image/x-icon" />
+  <link rel="apple-touch-icon" sizes="57x57" href="/static/images/favicon/apple-icon-57x57.png" />
+  <link rel="apple-touch-icon" sizes="60x60" href="/static/images/favicon/apple-icon-60x60.png" />
+  <link rel="apple-touch-icon" sizes="72x72" href="/static/images/favicon/apple-icon-72x72.png" />
+  <link rel="apple-touch-icon" sizes="76x76" href="/static/images/favicon/apple-icon-76x76.png" />
+  <link rel="apple-touch-icon" sizes="114x114" href="/static/images/favicon/apple-icon-114x114.png" />
+  <link rel="apple-touch-icon" sizes="120x120" href="/static/images/favicon/apple-icon-120x120.png" />
+  <link rel="apple-touch-icon" sizes="144x144" href="/static/images/favicon/apple-icon-144x144.png" />
+  <link rel="apple-touch-icon" sizes="152x152" href="/static/images/favicon/apple-icon-152x152.png" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/static/images/favicon/apple-icon-180x180.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="/static/images/favicon/favicon-16x16.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="/static/images/favicon/favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="96x96" href="/static/images/favicon/favicon-96x96.png" />
+  <link rel="icon" type="image/png" sizes="192x192" href="/static/images/favicon/android-icon-192x192.png" />
+
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="viewport" content="initial-scale=1,user-scalable=no" />
+
+  <meta name="description"
+    content="The Secret Lab crafts solutions for the new digital world. With equality for people, nature, and creativity. That is our chemistry of joy." />
+  <meta name="robots" content="noodp" />
+  <link rel="canonical" href="https://thesecretlab.nl" />
+  <meta property="og:locale" content="en_GB" />
+  <meta property="og:type" content="website" />
+  <meta property="og:title" content="The secret lab" />
+  <meta property="og:description"
+    content="The Secret Lab crafts solutions for the new digital world. With equality for people, nature, and creativity. That is our chemistry of joy." />
+  <meta property="og:url" content="https://thesecretlab.nl" />
+  <meta property="og:site_name" content="The secret lab" />
+  <meta property="og:image" content="https://thesecretlab.nl/assets/images/fb-card.png" />
+  <meta property="og:image:width" content="1280" />
+  <meta property="og:image:height" content="720" />
+  <meta name="twitter:card" content="photo" />
+  <meta name="twitter:url" content="https://thesecretlab.nl" />
+  <meta name="twitter:description"
+    content="The Secret Lab crafts solutions for the new digital world. With equality for people, nature, and creativity. That is our chemistry of joy." />
+  <meta name="twitter:title" content="The secret lab" />
+  <meta name="twitter:image" content="https://thesecretlab.nl/assets/images/twitter-card.png" />
+    </Head>
+  )
+};
+
+export default Meta;
